e7dde7d5b9895940aa405d333b39642ea516e279
Shepel Vladislav <sh3pelvlad@gmail.com>
2024-09-13 10:21:49 +0000

    Update solution

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,7 +1,8 @@
 cd data
 unzip archive-part2.zip -d archive_d
 tar -xf archive-part1.tar -C archive_d
-tar -zcf archive-combined.tar.gz archive_d
-
+cd archive_d
+tar -zcf archive-combined.tar.gz some
+mv archive-combined.tar.gz ..
+cd ..
 rm -rf archive_d
-
